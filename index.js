11140855b0f9feb27eb8d507de6749728eafa2e4
isakibul15 <sakibulislam55@iut-dhaka.edu>
2023-07-31 10:24:08 +0600

    Project Initialize and Player Creation With Gravity

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;  
 
+const gravity = 0.5 
 // es6 javascript class
 class Player{
     // constructor method
@@ -13,15 +14,60 @@ class Player{
             x: 100,
             y: 100
         }
-        this.width = 100
-        this.height = 100
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.width = 30
+        this.height = 30
     }
 
     draw() {
+        c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height) 
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        
+        if( this.position.y + this.height + this.velocity.y <= canvas.height){
+            this.velocity.y += gravity
+        }
+        else{
+            this.velocity.y = 0
+        }
     }
 }
 
 
 const player = new Player()
-player.draw()
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.update()
+}
+
+animate()
+
+ addEventListener('keydown',({ keyCode }) => {
+    //  
+    switch(keyCode){
+        case 65:  
+            console.log('left')
+            break
+        case 83:  
+            console.log('down')
+            break
+        case 68:  
+            console.log('right')
+            player.velocity.x += 1 
+            break
+        case 87:  
+            console.log('up')
+            player.velocity.y -= 20
+            break
+    }        
+} ) 
